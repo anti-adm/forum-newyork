74bf7a11904a2ac88188606d367bacec5d4565d0
Klym Z <klymz@anti.local>
2025-12-06 16:33:26 +0100

    fix: puppeteer no-sandbox and bot token

--- forum-bot/scan-active-complaints.js
+++ forum-bot/scan-active-complaints.js
@@ -274,6 +274,10 @@ async function main() {
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: null,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+    ],
   });
 
   const page = await browser.newPage();
